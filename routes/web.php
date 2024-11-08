a0fd414e95c5c96f6b5cc0f1c99e8799bfd534f3
fabiou-r <fabianandresmolinagarcia1@gmail.com>
2024-11-08 18:22:28 -0500

    fix:web.php routes delete of google

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-
 Route::redirect('/', '/dashboard');
 
 
@@ -19,52 +14,3 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('/login-google', function () {
-    return Socialite::driver('google')->redirect();
-});
-
-Route::get('/google-callback', function () {
-    try {
-        $user = Socialite::driver('google')->user();
-
-        /*verificar si el usuario existe usando
-        el id del proveedor de google y el external auth
-        */
-
-        $userExist = User::where('provider_id', $user->id)
-            ->where('external_auth', 'google')
-            ->first();
-
-        if ($userExist) {
-
-            Auth::login($userExist);
-        } else {
-       /* Si no existe
-        * se crea un nuevo
-        *  usuario en la base de datos
-        *
-        *  */
-            $userNew = User::create([
-                'name' => $user->name,
-                'email' => $user->email,
-                'avatar' => $user->avatar,
-                'provider_id' => $user->id,
-                'external_auth' => 'google',
-            ]);
-
-            /* Se le Asigna un rol
-            * de cliente por defecto
-            */
-            $userNew->assignRole('client');
-            Auth::login($userNew);
-        }
-
-       /*  Redirigir a la página de dashboard*/
-        return redirect('/dashboard');
-    } catch (Exception $e) {
-        /*Se maneja el error de autenticación con google */
-        Log::error('Error en la autenticación: ' . $e->getMessage());
-
-        return redirect('/')->with('error', 'Error al iniciar sesión con Google.');
-    }
-});
