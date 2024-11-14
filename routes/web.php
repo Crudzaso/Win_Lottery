eed1a788b4112e81e44df141713d233a3ced5ed1
fabiou-r <fabianandresmolinagarcia1@gmail.com>
2024-11-14 16:06:02 -0500

    fix:add route in web.php and force https in appserviceprovider.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
+
 
 Route::middleware([
     'auth:sanctum',
