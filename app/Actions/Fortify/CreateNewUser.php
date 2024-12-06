ac2b7a863c2fd354a35b195a97821cfcaed42f87
Julian Sneider <juliansnei@gmail.com>
2024-12-06 18:25:59 -0500

    fix: fixing login and register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,16 +22,18 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'username' => ['nulleable', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
+        $username = $input['username'] ?? null;
+
         return User::create([
             'first_name' => $input['first_name'],
             'last_name' => $input['last_name'],
-            'username' => $input['username'],
+            'username' => $username,
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
